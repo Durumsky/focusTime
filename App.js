bc952d2664295668a694accd760c773c996ade17
Jones Durumsky <jonesdurumsky@gmail.com>
2022-01-24 15:46:35 +0100

    starting the basic structure of the app

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50,
+  },
+});
